e94df49d1da24ec96f2d0c7c4be3cbaed09c4745
Jeremy Fischer <jefische86@gmail.com>
2025-01-21 12:47:07 -0500

    popup styling and geojson updates

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Virginia Mapping Project
 
-County data is pulled from: https://gist.github.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a 
+County geojson data is pulled from: https://gist.github.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a 
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,7 @@
-import county_list from '../assets/usa_counties.js';
+import county_list from '../assets/data/usa_counties.js';
 
 var VA_counties = county_list.features.filter((x) => {
 	return x.properties.STATEFP == '51'; // Virginia
 })
+
+// console.log(VA_counties);
