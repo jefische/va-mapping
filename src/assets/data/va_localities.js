d3ecce8538e67dfc6799282f3f2ee0bcec1ce5fd
Jeremy Fischer <jefische86@gmail.com>
2025-02-20 11:03:10 -0500

    marker and label draggable updates

---
+++ src/assets/data/va_localities.js
@@ -0,0 +1,106 @@
+export default {
+	"type": "FeatureCollection",
+	"name": "Virginia Localities",
+	"features": [
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Albemarle",
+				"rcv": "adopted",
+				// "placement": "right",
+				// "isCounty": "county"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-78.65503, 38.16004],
+				"labelAdjustment": [0, 0]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Arlington",
+				"rcv": "adopted",
+				// "placement": "left",
+				// "isCounty": "county"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-77.10596, 38.88371],
+				"labelAdjustment": [0.39, 0.145]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Charlottesville",
+				"rcv": "adopted",
+				// "placement": "top",
+				// "isCounty": "city"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-78.47978, 38.03649],
+				"labelAdjustment": [0, 0]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Fairfax City",
+				"rcv": "adopted",
+				// "placement": "right",
+				// "isCounty": "city"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-77.29912, 38.85328],
+				"labelAdjustment": [0, 0]
+				// "coordinates": [-77.49912, 38.85328]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Falls Church",
+				"rcv": "adopted",
+				// "placement": "top",
+				// "isCounty": "county"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-77.17613, 38.88623],
+				"labelAdjustment": [0, 0]
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Loudoun",
+				"rcv": "considering",
+				// "placement": "right",
+				// "isCounty": "county"
+			},
+			"geometry": {
+				"type": "Point",
+				// "coordinates": [-77.60887, 39.06769],
+				"labelAdjustment": [0, 0],
+				"coordinates": [-77.65887, 39.18869],
+			}
+		},
+		{
+			"type": "Feature",
+			"properties": {
+				"name": "Newport News",
+				"rcv": "used",
+				// "placement": "top",
+				// "isCounty": "county"
+			},
+			"geometry": {
+				"type": "Point",
+				"coordinates": [-76.49012, 37.08452],
+				"labelAdjustment": [0, 0]
+			}
+		},
+	]
+}
