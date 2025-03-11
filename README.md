b8dd07cce0ef08b458190a3d4323dbda666ceff5
Jeremy Fischer <jefische86@gmail.com>
2025-03-10 20:51:07 -0400

    mapping edits

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Virginia Mapping Project
 
-County geojson data is pulled from: https://gist.github.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a 
+County geojson data is pulled from: https://gist.github.com/sdwfrost/d1c73f91dd9d175998ed166eb216994a
+
+County and City marker coordinates pulled from: https://public.opendatasoft.com/explore/dataset/us-county-boundaries/export/?flg=en-us&disjunctive.stusab&disjunctive.statefp&disjunctive.countyfp&disjunctive.name&disjunctive.namelsad&disjunctive.state_name&refine.statefp=51
--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-import county_list from '../assets/data/usa_counties.js';
-import va_localities_opendatasoft from '../assets/data/va_localities_opendatasoft.js';
+import county_list from "../assets/data/usa_counties.js";
+import va_localities_opendatasoft from "../assets/data/va_localities_opendatasoft.js";
 
 var VA_counties = county_list.features.filter((x) => {
-	return x.properties.STATEFP == '51'; // Virginia
-})
+	return x.properties.STATEFP == "51"; // Virginia
+});
 
 // console.log(VA_counties.length);
 // console.log(VA_counties[30]);
@@ -13,26 +13,26 @@ for (var i = 0; i < VA_counties.length; i++) {
 }
 
 const getLocationData = () => {
-	const locationElements = document.querySelectorAll('#va-county-data .rcva-location');
+	const locationElements = document.querySelectorAll("#va-county-data .rcva-location");
 	const locations = [];
 
-	locationElements.forEach(element => {
-		const name = element.getAttribute('data-name');
-		const description = element.getAttribute('data-description');
+	locationElements.forEach((element) => {
+		const name = element.getAttribute("data-name");
+		const description = element.getAttribute("data-description");
 		let background = name.replace(" ", "-").toLowerCase() + "-background";
 
 		element.children[0].children[0].classList.add(background);
 		const body = element.children[0];
-		
+
 		// console.log(test);
 		// console.log(body.outerHTML);
-		locations.push({name, description, body});
+		locations.push({ name, description, body });
 	});
 	return locations;
 };
 
-const locations = getLocationData(); 
-console.log('number of counties and cities: ' + va_localities_opendatasoft.features.length)
+const locations = getLocationData();
+console.log("number of counties and cities: " + va_localities_opendatasoft.features.length);
 
 let sortedCounties = va_localities_opendatasoft.features.sort((a, b) => {
 	if (a.name > b.name) {
@@ -40,10 +40,10 @@ let sortedCounties = va_localities_opendatasoft.features.sort((a, b) => {
 	} else {
 		return -1;
 	}
-})
+});
 
 let j = 1;
 for (let feature of sortedCounties) {
-	console.log(j + ": " + feature.namelsad);
+	// console.log(j + ": " + feature.namelsad);
 	j++;
 }
